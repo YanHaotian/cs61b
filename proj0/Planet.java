50485c3fd14c4ae75107c728bbfa5195065e3205
YanHaotian <894796940@qq.com>
2020-11-02 15:38:45 +0800

    add the Planet.java

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,4 +1,28 @@
-package proj0;
-
 public class Planet {
+    public double xxPos;   // current x position.
+    public double yyPos;   // current y position.
+    public double xxVel;   // current velocity in the x direction.
+    public double yyVel;   // current velocity in the y direction.
+    public double mass;    // mass.
+    public String imgFileName;     // The name of the file that corresponds to the image that depicts the planet.
+
+
+    public Planet(double xP, double yP, double xV, double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    public Planet(Planet p) {
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName = p.imgFileName;
+    }
+
 }
