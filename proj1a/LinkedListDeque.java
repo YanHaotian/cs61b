653b5e41300bb6dc22c83f87c7da705e68d0f9c1
YanHaotian <894796940@qq.com>
2020-11-12 16:59:52 +0800

    Finished LinkedListDeque in a correct way.

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,97 +1,101 @@
 public class LinkedListDeque<T> implements DequeAPI<T> {
-    /** One single node of the linked list deque.*/
+    /** 错了，这个就是链表，不是队列！！.*/
     private static class Node<T> {
         public T data;
+        public Node<T> prev;
         public Node<T> next;
 
-        public Node(T d, Node<T> n) {
+        public Node(T d, Node<T> p,Node<T> n) {
             data = d;
+            prev = p;
             next = n;
         }
     }
 
 
-    public Node<T> first;
+    public Node<T> sentinel;
     public int size;
 
+
     public LinkedListDeque() {
-        first = null;
+        sentinel = new Node<>(null, null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
         size = 0;
     }
 
 
     public void addFirst(T item) {
-        first = new Node<>(item, first);
+        sentinel.next = new Node<>(item, sentinel, sentinel.next);
+        sentinel.next.next.prev = sentinel.next;
         size += 1;
     }
+
 
     public void addLast(T item) {
-        Node<T> p = first;
-        while (p.next != null) {
-            p = p.next;
-        }
-        p.next = new Node<>(item, null);
-        // first = p; why???
+        sentinel.prev = new Node<>(item, sentinel.prev, sentinel);
+        sentinel.prev.prev.next = sentinel.prev;
         size += 1;
 
     }
+
 
     public boolean isEmpty() {
         return size == 0;
     }
 
+
     public int size() {
         return size;
     }
 
+
     public void printDeque() {
-        if (first == null) {
+        if (sentinel == null) {
             return;
         }
-
-        Node<T> temp = first;
+        Node<T> temp = sentinel;
         for (int i=0; i<size; i+=1) {
             System.out.println(temp.data);
             temp = temp.next;
         }
     }
 
+
     public T removeFirst() {
-        T removedData = first.data;
-        first = first.next;
+        if (size == 0) {
+            return null;
+        }
+        T removedData = sentinel.next.data;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
         size -= 1;
         return removedData;
     }
 
 
-    private Node<T> getLastNode() {
-        Node<T> p = first;
-        while (p.next != null) {
-            p = p.next;
-        }
-        return p;
-    }
-
     public T removeLast() {
-        Node<T> back = getLastNode();
-
         // Do not use "first = first.next" directly,
         // use it on a duplicate node p instead.
-        Node<T> p = first;
-        while (p.next != back) {
-            p = p.next;
+        if (size == 0) {
+            return null;
         }
-        p.next = null;
-        size -= 1;
-        return back.data;
+        T removedData = sentinel.prev.data;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        return removedData;
     }
 
+
     public T get(int index) {
-        Node<T> p = first;
-        while (index > 0) {
-            p = p.next;
+        if (index < 0) {
+            return null;
+        }
+        Node<T> pointer = sentinel;
+        while (index >= 0) {
+            pointer = pointer.next;
             index -= 1;
         }
-        return p.data;
+        return pointer.data;
     }
 }
